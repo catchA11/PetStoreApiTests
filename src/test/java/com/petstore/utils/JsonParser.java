0584b52d266e5a384ba88547df876fc71f92e53e
Lucy Sheehy <lucysheehy@yahoo.com>
2023-02-26 20:52:14 +0000

    add logging to JsonParser

--- src/test/java/com/petstore/utils/JsonParser.java
+++ src/test/java/com/petstore/utils/JsonParser.java
@@ -2,14 +2,18 @@ package com.petstore.utils;
 
 import org.json.JSONArray;
 import org.json.JSONTokener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Objects;
 
 public class JsonParser {
     public JSONArray jsonArray;
+    private static final Logger log = LoggerFactory.getLogger(JsonFilters.class);
 
     public JSONArray setJSON(String path) {
         File jsonFile = new File(System.getProperty("user.dir") + "/src/test/resources/" + path);
@@ -18,13 +22,14 @@ public class JsonParser {
             inputStream = new FileInputStream(jsonFile);
         }
         catch (FileNotFoundException e) {
+            log.error("pets.json file not found at " + jsonFile);
             e.printStackTrace();
         }
         JSONTokener jsonTokener = null;
         if (inputStream != null) {
             jsonTokener = new JSONTokener(inputStream);
         }
-        jsonArray = new JSONArray(jsonTokener);
+        jsonArray = new JSONArray(Objects.requireNonNull(jsonTokener));
         return jsonArray;
     }
 }
